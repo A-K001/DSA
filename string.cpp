cee23a4c6d0a0bf1439207528abaff80f8ffb9e2
Aayush Kumar <kumaraayush835@gmail.com>
2025-04-26 02:30:06 +0530

    adding reverse string function

--- string.cpp
+++ string.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
-
+void reverseArray (char name[],int len){
+    int s = 0;
+    int e = len - 1;
+    while (s<e){
+        swap(name[s++],name[e--]);
+    }
+}
 int getLength(char name[]){
     
     int i = 0;
@@ -15,7 +21,11 @@ int main() {
     cout << "Enter your First name : " ;
     cin >> name;
     cout << "Your name is "<< name << endl;
-    cout << "Length of ur name is : " << getLength(name) << endl ;
-    
+    int len = getLength(name);
+    cout << "Length of ur name is : " << len << endl ;
+    reverseArray(name,len);
+    cout << "Reversed name : " << name << endl;
+
     return 0;
 }
+
